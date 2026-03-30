ee1ce342610f76f9df40c82e33772de332e51a50
Zhijie (Jack) Rong <jackrong03@gmail.com>
2026-03-30 21:20:08 +0100

    Add demo file for LSTM

--- src/lstm_utils/lstm_dataset.py
+++ src/lstm_utils/lstm_dataset.py
@@ -9,7 +9,7 @@ from src.lstm_utils.lstm_tokeniser import LSTMTokeniser
 
 
 class LSTMDataset(Dataset):
-    def __init__(self, csv_path: Path, tokeniser: LSTMTokeniser):
+    def __init__(self, csv_path: Path, tokeniser: LSTMTokeniser, evaluate: bool = False):
         df = pd.read_csv(csv_path)
 
         premise_pairs = [tokeniser.encode(text, CONFIG.lstm.max_length) for text in df['premise']]
@@ -19,16 +19,21 @@ class LSTMDataset(Dataset):
         hypothesis_pairs = [tokeniser.encode(text, CONFIG.lstm.max_length) for text in df['hypothesis']]
         self.hypothesis_ids = torch.tensor([pair[0] for pair in hypothesis_pairs])
         self.hypothesis_masks = torch.tensor([pair[1] for pair in hypothesis_pairs])
-        self.labels = torch.tensor(df['label'].values, dtype=torch.long)
+        if not evaluate:
+            self.labels = torch.tensor(df['label'].values, dtype=torch.long)
+        else:
+            self.labels = None
 
     def __len__(self) -> int:
-        return len(self.labels)
+        return len(self.premise_ids)
 
     def __getitem__(self, idx) -> dict[str, torch.Tensor]:
-        return {
+        item = {
             'premise_ids': self.premise_ids[idx],
             'premise_mask': self.premise_masks[idx],
             'hypothesis_ids': self.hypothesis_ids[idx],
             'hypothesis_mask': self.hypothesis_masks[idx],
-            'label': self.labels[idx],
         }
+        if self.labels is not None:
+            item['label'] = self.labels[idx]
+        return item
